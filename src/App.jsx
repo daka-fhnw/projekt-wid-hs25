161b72fe843858ca2f3962f3020df3be33ddeef8
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2025-11-14 18:29:48 +0100

    MUI hinzugefügt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Header } from "./Header.jsx";
+import { Home } from "./Home.jsx";
 import { Chart } from "./Chart.jsx";
 import { useState } from "react";
 
@@ -9,7 +10,7 @@ function App() {
     <>
       <Header setPage={setPage} />
       <main>
-        {page === 0 && <div>Page 1</div>}
+        {page === 0 && <Home />}
         {page === 1 && <Chart />}
         {page === 2 && <div>Page 3</div>}
       </main>
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import Favorite from "@mui/icons-material/Favorite";
+import { Button } from "@mui/material";
+
+export function Home() {
+  return (
+    <>
+      <div>
+        <Button variant="contained">MUI Test</Button> <Favorite />
+      </div>
+    </>
+  );
+}
