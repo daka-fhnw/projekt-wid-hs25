8ffbd88e0aab623794f5988932d206c50eb2d73d
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2025-11-19 15:41:56 +0100

    Header Logo und Style angepasst

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,17 +3,16 @@ import logo from "./assets/FHNW_Logo.jpg";
 export function Header({ page, setPage }) {
   return (
     <header>
-      <div id="Elternelement1">
+      <div className="elternelement1">
         <img
           onClick={() => setPage(0)}
           src={logo}
-          width="150"
-          height="100"
+          width="100"
           title="Logo FHNW"
         />
         <h1>Passantenfrequenz entlang der Bahnhofsstrasse Zürich</h1>
       </div>
-      <div id="Elternelement2">
+      <div className="elternelement2">
         <button
           onClick={() => setPage(0)}
           className={page === 0 ? "active" : ""}
