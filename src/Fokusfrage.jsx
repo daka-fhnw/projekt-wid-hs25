a16fa4f7aed058ab63f90febe3eae1501e968938
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2025-11-27 22:28:40 +0100

    Code aufgeräumt

--- src/Fokusfrage.jsx
+++ src/Fokusfrage.jsx
@@ -4,26 +4,26 @@ import fokusfrage from "./assets/fokusfrage.json";
 export function Fokusfrage() {
   return (
     <>
-      <div className="Ueberschrift">
-        Wo waren im April 2024 die meisten Kinder unterwegs?
-      </div>
-      <p></p>
-      <br></br>
+      <h2>Die Fokusfrage</h2>
+      <p>Wo waren im April 2024 die meisten Kinder unterwegs?</p>
+
       <VegaEmbed spec={fokusfrage} />
-      <div className="Ueberschrift">Analysevorgang</div>
-      <p></p>
-      <div className="Text">
-        1.) Filtern des gesamten Datensatzes und Auswahl der Daten im Zeitraum
-        vom 1. bis 30. April 2024.
-      </div>
-      <div className="Text">
-        2.) Erstellung einer Pivot-Tabelle, die für jede Kombination der
-        Messgebiete aggregierte Daten zu den Fussgängerzahlen enthält.
-      </div>
-      <div className="Text">
-        3.) Berechnung des Anteils von Kindern und Erwachsenen an der Gesamtzahl
-        der Fussgänger für jeden dieser Standorte.
-      </div>
+
+      <h3>Analysevorgang</h3>
+      <ol>
+        <li>
+          Filtern des gesamten Datensatzes und Auswahl der Daten im Zeitraum vom
+          1. bis 30. April 2024.
+        </li>
+        <li>
+          Erstellung einer Pivot-Tabelle, die für jede Kombination der
+          Messgebiete aggregierte Daten zu den Fussgängerzahlen enthält.
+        </li>
+        <li>
+          Berechnung des Anteils von Kindern und Erwachsenen an der Gesamtzahl
+          der Fussgänger für jeden dieser Standorte.
+        </li>
+      </ol>
     </>
   );
 }
