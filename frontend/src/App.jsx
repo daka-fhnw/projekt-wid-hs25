781f080c770e0c0413cdadc6a4791c83d2b13217
Daniel Käser <daniel.kaeser@students.fhnw.ch>
2025-12-16 13:51:21 +0100

    MUI integriert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import { useState } from "react";
 import { Header } from "./Header.jsx";
--- frontend/src/Erkundung.jsx
+++ frontend/src/Erkundung.jsx
@@ -1,7 +1,10 @@
+import Button from "@mui/material/Button";
+
 export function Erkundung() {
   return (
     <>
       <h2>Die Erkundung</h2>
+      <Button variant="contained">Contained</Button>
     </>
   );
 }
